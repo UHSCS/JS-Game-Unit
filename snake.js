69cd985eb8e6cae65a9fc2561298270841d37aa5
Patrick Liu <brogrammer0111@gmail.com>
2019-01-16 17:23:27 -0800

    Update snake.js

--- snake.js
+++ snake.js
@@ -4,6 +4,8 @@ function setup() {
  
   createCanvas(1000, 800);
   
+	move_tick = 20
+	dir = "up"
   
   //Snake array, organized by center x and y coords of each square
   snake = [500, 340,
@@ -13,6 +15,22 @@ function setup() {
            500, 500,
            500, 540]
 }
+
+	function keyPressed(){	
+	if(keyCode == UP_ARROW) {
+		dir = "up"
+	}
+	else if(keyCode == DOWN_ARROW) {
+		dir = "down"
+	}
+	else if(keyCode == LEFT_ARROW) {
+		dir = "left"
+	}
+	else if(keyCode == RIGHT_ARROW) {
+		dir = "right"
+	}
+}
+
 
 function draw() {
     //background 
@@ -32,10 +50,16 @@ function draw() {
   for(i = snake.length; i > 0; i -= 2){
   	rect(snake[i]-20, snake[i+1]-20, 40, 40)
   }
-
-snake.pop()
-snake.pop()
-snake.unshift(snake[0], snake[1]-40)
-
+	
+	move_tick = move_tick - 1
+	if( move_tick == 0){
+		move_tick = 20
+		snake.pop()
+		snake.pop()
+		if(dir == "up"){snake.unshift(snake[0], snake[1]-40)}
+		else if(dir == "down"){snake.unshift(snake[0], snake[1]+40)}
+		else if(dir == "left"){snake.unshift(snake[0]-40, snake[1])}
+		else if(dir == "right"){snake.unshift(snake[0]+40, snake[1])}
+	}
 
 }
