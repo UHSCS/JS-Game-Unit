d4994e7585332946c4739d1adb0e1af3b70cfc56
Patrick Liu <brogrammer0111@gmail.com>
2019-01-16 19:11:49 -0800

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,39 +1,44 @@
-function setup() {
-     
+function setup() {  
   //mandatory lines
- 
+  noStroke()
   createCanvas(1000, 800);
-  
-	move_tick = 20
-	dir = "up"
-  
-  //Snake array, organized by center x and y coords of each square
+
+	//snake array, organized by center x and y coords of each square
   snake = [500, 340,
            500, 380,
            500, 420,
            500, 460,
            500, 500,
            500, 540]
+
+  //control variables
+  dir = "up"
+	move_tick = 10
+
+}
+	
+//key controls
+function keyPressed() {
+        
+        //link keys to variable
+        if (keyCode === UP_ARROW && dir != "down"){
+          dir = "up"
+        }
+        if (keyCode === DOWN_ARROW && dir != "up"){
+          dir = "down"
+        }
+        if (keyCode === RIGHT_ARROW && dir != "left"){
+          dir = "right"
+        }
+        if (keyCode === LEFT_ARROW && dir != "right"){
+          dir = "left"
+        }
 }
 
-	function keyPressed(){	
-	if(keyCode == UP_ARROW) {
-		dir = "up"
-	}
-	else if(keyCode == DOWN_ARROW) {
-		dir = "down"
-	}
-	else if(keyCode == LEFT_ARROW) {
-		dir = "left"
-	}
-	else if(keyCode == RIGHT_ARROW) {
-		dir = "right"
-	}
-}
-
-
+//draw loop!
 function draw() {
-    //background 
+  
+  //background 
   background(240,240,240);
   
   //lattice lines
@@ -44,22 +49,41 @@ function draw() {
   for(i = 0; i < 800; i += 40){
   	rect(0, i-1, 1000, 2)
   }  
-	
-	  //body
+  
+  //snake graphics
   fill(150,150,150)
   for(i = snake.length; i > 0; i -= 2){
   	rect(snake[i]-20, snake[i+1]-20, 40, 40)
   }
-	
-	move_tick = move_tick - 1
-	if( move_tick == 0){
-		move_tick = 20
-		snake.pop()
-		snake.pop()
-		if(dir == "up"){snake.unshift(snake[0], snake[1]-40)}
-		else if(dir == "down"){snake.unshift(snake[0], snake[1]+40)}
-		else if(dir == "left"){snake.unshift(snake[0]-40, snake[1])}
-		else if(dir == "right"){snake.unshift(snake[0]+40, snake[1])}
-	}
+
+  //snake movement  
+  if(move_tick == 10){
+    	
+    //always delete the last two positions 		
+    snake.pop()
+    snake.pop() 
+    
+    //movement, link dir variable to snake array
+    if(dir == "up"){
+			snake.unshift(snake[0], snake[1]-40)
+    }else if(dir == "down"){
+			snake.unshift(snake[0], snake[1]+40)
+    }else if(dir == "right"){
+			snake.unshift(snake[0]+40, snake[1])
+    }else{
+      //left
+			snake.unshift(snake[0]-40, snake[1])
+    }
+  }
+
+  //tick snake movement
+  move_tick --;
+  if (move_tick == 0){
+     move_tick = 10
+  }
 
 }
+
+
+
+
